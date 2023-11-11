9fd834537a5828cc16aca5bbf1a6648a94e3f071
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2023-11-11 18:01:46 +0500

    Dark

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let modeBtn = document.getElementById("mode-btn");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "light") {
+    this.firstElementChild.src = "images/dark.svg";
+  } else {
+    this.firstElementChild.src = "images/light.svg";
+  }
+  document.body.classList.toggle("light");
+});
+
+
+const loading = document.getElementById("loading");
+
+window.addEventListener("load", () => {
+  loading.classList.add('loading-none');
+});
