098a04f2869646c9b49627367dc9a764d1d37750
IbrokhimShukurullayev <ibrokhimjonshukurullayev>
2023-11-11 23:31:07 +0500

    dark

--- js/main.js
+++ js/main.js
@@ -9,6 +9,23 @@ modeBtn.addEventListener("click", function () {
   document.body.classList.toggle("light");
 });
 
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.classList.add('backtop-show')
+  } else {
+    backtop.classList.remove('backtop-show')
+  }
+}
+
 
 const loading = document.getElementById("loading");
 
